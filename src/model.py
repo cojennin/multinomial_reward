0f6f32bea948246b2b8c29d6680afcc0374b57e6
Connor Jennings <cojennin@gmail.com>
2023-05-01 12:41:35 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -109,8 +109,8 @@ class Model(nn.Module):
         inds = (input1 == self.PAD_ID).nonzero()
         return inds[0].item() if len(inds) > 0 else len(input1) - 1
 
-    def loss(self, outputs, batch):
-        return outputs["loss"]
+#     def loss(self, outputs, batch):
+#         return outputs["loss"]
 
 
 def all_pairs(n):
