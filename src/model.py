695d3d2498c53ca59c18823a88b8695d2885e3b6
Connor Jennings <cojennin@gmail.com>
2023-05-01 14:04:22 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -58,7 +58,8 @@ class Model(nn.Module):
 
         print("After end scores")
 
-        loss = torch.rand(1, device=self.transformer.device, requires_grad=True)
+#         loss = torch.rand(1, device=self.transformer.device, requires_grad=True)
+        loss = torch.tensor(0.0, requires_grad=True).to(self.transformer.device)
         
         return {
             "loss": loss,
